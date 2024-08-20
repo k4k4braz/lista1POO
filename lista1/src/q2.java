32b8732adf4f922879ff1a5cf3191dacfdba15ac
k4k4braz <kauevraz123@gmail.com>
2024-08-20 19:42:43 -0300

    questoes_lista1

---
+++ lista1/src/q2.java
@@ -0,0 +1,10 @@
+public class q2 {
+    public static void main(String [] args){
+        int numero1 = 10;
+        int numero2 = 10;
+
+        int soma = numero1 + numero2;
+
+        System.out.println("a soma e" + soma);
+    }
+}
