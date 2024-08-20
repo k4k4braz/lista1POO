2b42626fa828fcc7e2b690faa97be392e1ee7fed
k4k4braz <kauevraz123@gmail.com>
2024-08-20 19:31:18 -0300

    questoes_lista1

--- src/kaka2.java
+++ src/kaka2.java
@@ -1,12 +1,13 @@
-public class kaka2 { public static void main(String[] args) {
-    String nome = "kaue";
-    byte idade = 19;
-    boolean possuiCarro = true;
-    float salario = 21.44545545f;
-    //double salario2 =21.5;
-    System.out.printf("meu nome é %s e tenho %d anos",nome, idade);
-    System.out.printf(" meu salario: %2f\n" , salario);
-    System.out.printf("eu tenho um carro: %b", possuiCarro);
-}
+public class kaka2 {
+    public static void main(String[] args) {
+        String nome = "kaue";
+        byte idade = 19;
+        boolean possuiCarro = true;
+        float salario = 21.44545545f;
+        //double salario2 =21.5;
+        System.out.printf("meu nome é %s e tenho %d anos", nome, idade);
+        System.out.printf(" meu salario: %2f\n", salario);
+        System.out.printf("eu tenho um carro: %b", possuiCarro);
+    }
 }
 
