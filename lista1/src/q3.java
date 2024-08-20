729e7c561938ef74ee2664c2ba357068dadf8945
k4k4braz <kauevraz123@gmail.com>
2024-08-20 20:42:41 -0300

    questoes_lista1

---
+++ lista1/src/q3.java
@@ -0,0 +1,7 @@
+public class q3 {
+    public static void main(String[] args){
+        double valor = 5.67;
+        double resultado = valor * 3;
+        System.out.println("o resultado e" + resultado);
+    }
+}
