ddb1f6cb246d8048ca22f430d041723546cd9d09
k4k4braz <kauevraz123@gmail.com>
2024-08-20 23:55:30 -0300

    questoes_lista1

---
+++ lista1/src/q5.java
@@ -0,0 +1,6 @@
+public class q5 {
+    public static void main(String[] args){
+        boolean receba = true;
+        System.out.println("o valor e: " + receba);
+    }
+}
---
+++ lista1/src/q6.java
@@ -0,0 +1,7 @@
+public class q6 {
+    public static void main(String[] args){
+        String nome = "kaue";
+        String palavra = nome + " e um caba massa";
+        System.out.println(palavra);
+    }
+}
